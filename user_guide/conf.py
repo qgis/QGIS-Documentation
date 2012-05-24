5bbb20513a64a261d6a11664b24a6862d5a7fa12
capooti <capooti@capooti-mint.(none)>
2012-05-24 14:57:10 +0200

    Better to name master document as index for apache benefit.

--- user_guide/conf.py
+++ user_guide/conf.py
@@ -35,7 +35,7 @@ source_suffix = '.rst'
 #source_encoding = 'utf-8'
 
 # The master toctree document.
-master_doc = 'user_guide'
+master_doc = 'index'
 
 # General information about the project.
 project = u'QGIS User Guide'
