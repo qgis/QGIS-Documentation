a21198c8169d65045b92b0401960115fdf642f5f
capooti <pcorti@gmail.com>
2012-05-30 11:31:32 +0200

    Now using the qgis-theme, looks nicer at this time.

--- source/conf.py
+++ source/conf.py
@@ -167,7 +167,7 @@ rst_epilog = """
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
 #html_theme = 'sphinxdoc'
-html_theme = 'linfiniti-sphinx-theme'
+html_theme = 'qgis-theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -175,7 +175,7 @@ html_theme = 'linfiniti-sphinx-theme'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['..']
+html_theme_path = ['../themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
