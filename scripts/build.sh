3483f85a8c01bce07e03b112132586d35f831dba
dassau <dassau@gbd-consult.de>
2012-10-11 16:37:13 +0200

    started updating the build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,16 @@
 LOGFILE=build.log
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-cd $DIR
+cd $DIR/..
 git pull
-. qgis-doc-env/bin/activate
-make clean
-make compile_messages >> $LOGFILE 2>&1
-make html >> $LOGFILE 2>&1
+sh scripts/pre_translate.sh >> $LOGFILE 2>&1
+sh scripts/post_translate.sh >> $LOGFILE 2>&1
+
+#LOGFILE=build.log
+#DIR="$( cd "$( dirname "$0" )" && pwd )"
+#cd $DIR
+#git pull
+#. qgis-doc-env/bin/activate
+#make clean
+#make compile_messages >> $LOGFILE 2>&1
+#make html >> $LOGFILE 2>&1
 
