e8b2cfa69bb166a35e1e157b43f036471173b836
dassau <dassau@gbd-consult.de>
2012-10-16 13:06:15 +0200

    updated build script for qgis.org

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,20 @@
 LOGFILE=build.log
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd $DIR/..
+
+# remove old po files
+for i in `find . -name *.po` ; do
+   rm -f $i
+done
+
+# get docs from git
 git pull
+
+# activate virtualenv
+. sphinx-env/bin/activate
+
+# build html and pdf for all LOCALES
 sh scripts/pre_translate.sh >> $LOGFILE 2>&1
 sh scripts/post_translate.sh >> $LOGFILE 2>&1
 
-#LOGFILE=build.log
-#DIR="$( cd "$( dirname "$0" )" && pwd )"
-#cd $DIR
-#git pull
-#. qgis-doc-env/bin/activate
-#make clean
-#make compile_messages >> $LOGFILE 2>&1
-#make html >> $LOGFILE 2>&1
 
