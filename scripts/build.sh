fdaf5d438eef4786a830b625f08e9b3ec95ce3b7
dassau <dassau@gbd-consult.de>
2012-10-30 13:25:28 +0100

    make server build again

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,18 +3,18 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd $DIR/..
 
 # remove old po files
-for i in `find . -name *.po` ; do
-   rm -f $i
-done
+#for i in `find . -name *.po` ; do
+#   rm -f $i
+#done
 
 # get docs from git
 git pull
 
 # activate virtualenv
-. sphinx-env/bin/activate
+#. sphinx-env/bin/activate
 
 # build html and pdf for all LOCALES
-sh scripts/pre_translate.sh >> $LOGFILE 2>&1
+# sh scripts/pre_translate.sh >> $LOGFILE 2>&1
 sh scripts/post_translate.sh >> $LOGFILE 2>&1
 
 
