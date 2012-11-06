551d40aecea2056099b3f386f2329267e6a5b3d4
Richard Duivenvoorde <richard@duif.net>
2012-11-06 13:13:33 +0100

    renaming theme

--- source/conf.py
+++ source/conf.py
@@ -592,7 +592,7 @@ rst_epilog = """
 #html_theme = 'basic'
 #html_theme = 'sphinxdoc'
 #html_theme = 'qgis-theme'
-html_theme = 'test-theme'
+html_theme = 'qgis2-theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
