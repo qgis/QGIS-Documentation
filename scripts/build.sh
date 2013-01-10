84d5f973aaa17da98270a677c5a46b76ef5b18df
dassau <dassau@gbd-consult.de>
2013-01-10 10:19:33 +0100

    update script to create certain language in virt sphinx env

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,12 @@
+#!/bin/sh
+# Script to create translation files and docs for all or 
+# a certain, available language (e.g. it,nl,de,...)
+# USAGE: sh ./scripts/build.sh it
+
 LOGFILE=build.log
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd $DIR/..
+LANGUAGE=$1
 
 # remove old po files
 #for i in `find . -name *.po` ; do
@@ -10,11 +16,10 @@ cd $DIR/..
 # get docs from git
 git pull
 
-# activate virtualenv
+# activate virtualenv (comment if sphinx is installed locally)
 . sphinx-env/bin/activate
 
 # build html and pdf for all LOCALES
-sh scripts/pre_translate.sh >> $LOGFILE 2>&1
-sh scripts/post_translate.sh >> $LOGFILE 2>&1
-
+sh scripts/pre_translate.sh $LANGUAGE >> $LOGFILE 2>&1
+sh scripts/post_translate.sh $LANGUAGE >> $LOGFILE 2>&1
 
