784dd8d759936c71209220c3f7af0386498e49ff
dassau <dassau@gbd-consult.de>
2013-01-10 11:02:49 +0100

    distiguish virtual and local sphinx env

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,23 +3,28 @@
 # a certain, available language (e.g. it,nl,de,...)
 # USAGE: sh ./scripts/build.sh it
 
-LOGFILE=build.log
+LOGFILE=build$(date +\%Y\%m\%d\-%H\%M).log
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd $DIR/..
-LANGUAGE=$1
 
-# remove old po files
-#for i in `find . -name *.po` ; do
-#   rm -f $i
-#done
+## check for language
+if [ $1 ]; then
+   LOCALES=$1
+fi
 
 # get docs from git
 git pull
 
-# activate virtualenv (comment if sphinx is installed locally)
-. sphinx-env/bin/activate
+## check, how sphinx is installed
+if [ `which sphinx-build` ]; then
+   SPHINXBUILD=`which sphinx-build`
+else
+   ## activate virtualenv
+   . sphinx-env/bin/activate
+fi
 
-# build html and pdf for all LOCALES
-sh scripts/pre_translate.sh $LANGUAGE >> $LOGFILE 2>&1
-sh scripts/post_translate.sh $LANGUAGE >> $LOGFILE 2>&1
+# build translation files and docs 
+sh scripts/pre_translate.sh $LOCALES >> /tmp/$LOGFILE 2>&1
+sh scripts/post_translate.sh $LOCALES >> /tmp/$LOGFILE 2>&1
 
+echo "Finished. Logfile /tmp/$LOGFILE added."
