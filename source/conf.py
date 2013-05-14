a9bb684be614472522b37e2b8c8b7b4948256f42
dassau <dassau@gbd-consult.de>
2013-05-14 17:02:45 +0200

    update year

--- source/conf.py
+++ source/conf.py
@@ -39,7 +39,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'QGIS Documentation Project'
-copyright = u'2012, QGIS project'
+copyright = u'2013, QGIS project'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
