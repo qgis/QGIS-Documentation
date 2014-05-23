406f2ce8061b646fba89462a76599bbc12ead7ca
Richard Duivenvoorde <richard@duif.net>
2014-05-23 11:15:43 +0200

    po files from first build of 2.2

--- docker-world.sh
+++ docker-world.sh
@@ -1,6 +1,7 @@
 
 # only languages which have translations in transifex
-for LANG in ca de en es fr gl hi id it ja km ko nl pl pt_BR pt_PT ro ru uk
+# nqo is not available in python :-(
+for LANG in en de es fr gl hi id it ja ko nl pl pt_BR pt_PT ro ru
   do
     /bin/bash ./docker-run.sh full LANG=$LANG
     rsync -hvrz -e ssh --progress output/pdf/$LANG qgis.osgeo.osuosl.org:/var/www/documentation/github/QGIS-Documentation-2.2/live/html/pdf
