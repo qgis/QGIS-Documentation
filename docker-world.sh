b3b22f08536de0d712e7e9571185c8d2e896ecb8
Richard Duivenvoorde <richard@duif.net>
2014-06-13 19:27:41 +0200

    fix rsync path

--- docker-world.sh
+++ docker-world.sh
@@ -5,6 +5,6 @@
 for LANG in en
   do
     /bin/bash ./docker-run.sh full LANG=$LANG
-    rsync -hvrz -e ssh --progress output/pdf/$LANG qgis.osgeo.osuosl.org:/var/www/documentation/github/QGIS-Documentation/live/html/pdf
-    rsync -hvrz -e ssh --progress output/html/$LANG qgis.osgeo.osuosl.org:/var/www/documentation/github/QGIS-Documentation/live/html
+    rsync -hvrz -e ssh --progress output/pdf/$LANG qgis.osgeo.osuosl.org:/var/www/qgisdata/QGIS-Documentation/live/html/pdf
+    rsync -hvrz -e ssh --progress output/html/$LANG qgis.osgeo.osuosl.org:/var/www/qgisdata/QGIS-Documentation/live/html
   done
