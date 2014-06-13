719e31677dac618d51d9c12fecfab956c5397c48
Richard Duivenvoorde <richard@duif.net>
2014-06-13 15:27:13 +0200

    fix format string, so it works on Windows too

--- source/conf.py
+++ source/conf.py
@@ -128,7 +128,7 @@ html_static_path = ['../themes/qgis-theme/static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-html_last_updated_fmt = '%k:%M %b %d, %Y'
+html_last_updated_fmt = '%H:%M %b %d, %Y'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
