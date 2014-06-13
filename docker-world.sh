a0bafa0359f0d3eee6b73129ad144ddedf5e3730
Richard Duivenvoorde <richard@duif.net>
2014-06-13 18:23:01 +0200

    fix

--- docker-world.sh
+++ docker-world.sh
@@ -8,6 +8,7 @@
 # only languages which have translations in transifex
 # nqo is not available in python :-(
 #for LANG in en de es fr gl hi id it ja ko nl pl pt_BR pt_PT ro ru
+for l in $langs
   do
     /bin/bash ./docker-run.sh full LANG=$LANG
     rsync -hvrz -e ssh --progress output/pdf/$LANG qgis.osgeo.osuosl.org:/var/www/documentation/github/QGIS-Documentation-2.2/live/html/pdf
