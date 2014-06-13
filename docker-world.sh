18eaad1bf4924f371f0731bd4edd8d1a026acb7e
Richard Duivenvoorde <richard@duif.net>
2014-06-13 18:06:28 +0200

    making it easier to docker build one language

--- docker-world.sh
+++ docker-world.sh
@@ -1,7 +1,13 @@
 
 # only languages which have translations in transifex
+: ${langs:=en de es fr gl hi id it ja ko nl pl pt_BR pt_PT ro ru}
+
+# if you only want to build one language, do:
+# $ langs=de ./docker-world.sh
+
+# only languages which have translations in transifex
 # nqo is not available in python :-(
-for LANG in en de es fr gl hi id it ja ko nl pl pt_BR pt_PT ro ru
+#for LANG in en de es fr gl hi id it ja ko nl pl pt_BR pt_PT ro ru
   do
     /bin/bash ./docker-run.sh full LANG=$LANG
     rsync -hvrz -e ssh --progress output/pdf/$LANG qgis.osgeo.osuosl.org:/var/www/documentation/github/QGIS-Documentation-2.2/live/html/pdf
