9790a6f3d64b8781b2151eb44aa60172cd0c16d7
Richard Duivenvoorde <richard@duif.net>
2014-08-11 15:34:47 +0200

    remove after succesfull build

--- docker-run.sh
+++ docker-run.sh
@@ -6,5 +6,5 @@
 
 cp ~/.transifexrc .
 pwd=$(pwd)
-docker run -v $pwd:/build -w="/build" qgis/sphinx:2 make $@
+docker run -v $pwd:/build -w="/build" --rm=true qgis/sphinx:2 make $@
 rm -rf .transifexrc
