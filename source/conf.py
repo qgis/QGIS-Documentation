efff9643c51e18654cf633dc6207a39179803242
dassau <dassau@gbd-consult.de>
2014-09-26 19:02:48 +0200

    fix icon

--- source/conf.py
+++ source/conf.py
@@ -1042,7 +1042,7 @@ rst_epilog = """
    :width: 1.5em
 .. |analysis| image:: /static/common/analysis.png
    :width: 1.5em
-.. |geRasterometry| image:: /static/common/geometry.png
+.. |geometry| image:: /static/common/geometry.png
    :width: 1.5em
 .. |geoprocessing| image:: /static/common/geoprocessing.png
    :width: 1.5em
