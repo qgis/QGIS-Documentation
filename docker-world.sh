59884e10d13a5615ef8e899fe5527253d3886491
Richard Duivenvoorde <richard@duif.net>
2014-12-02 13:41:26 +0100

    update for 2.6

--- docker-world.sh
+++ docker-world.sh
@@ -1,6 +1,4 @@
 
-# QGIS testing/master ONLY in english
-# translation is done only in latest stable
 
 # cd to script dir
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -11,9 +9,16 @@ git stash drop
 # get latest version
 git pull
 
-for LANG in en
+# only languages which have translations in transifex
+# nqo is not available in python :-(
+: ${langs:=en de es fi fr gl hi id it ja ko nl pl pt_BR pt_PT ro ru uk}
+
+# if you only want to build one language, do:
+# $ langs=de ./docker-world.sh
+
+for l in $langs
   do
-    /bin/bash ./docker-run.sh full LANG=$LANG
-    rsync -hvrz -e ssh --progress output/pdf/$LANG qgis.osgeo.osuosl.org:/var/www/qgisdata/QGIS-Documentation/live/html/pdf
-    rsync -hvrz -e ssh --progress output/html/$LANG qgis.osgeo.osuosl.org:/var/www/qgisdata/QGIS-Documentation/live/html
+    /bin/bash ./docker-run.sh full LANG=$l
+    rsync -hvrz -e ssh --progress output/pdf/$l qgis.osgeo.osuosl.org:/var/www/qgisdata/QGIS-Documentation-2.6/live/html/pdf
+    rsync -hvrz -e ssh --progress output/html/$l qgis.osgeo.osuosl.org:/var/www/qgisdata/QGIS-Documentation-2.6/live/html
   done
