44237f0361338d90bfd1be9b3436d47bdd345902
Richard Duivenvoorde <richard@duif.net>
2015-02-02 15:35:21 +0100

    adding some timing log + rsync on checksum

--- docker-world.sh
+++ docker-world.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+now =`date`
+echo "Starting $now"
+
 if [ -f running ]; then
 	echo "$0 still running"
 	exit 1
@@ -26,7 +29,7 @@ git pull
 
 for l in $langs
   do
-    /bin/bash ./docker-run.sh full LANG=$l
-    rsync -hvrz -e ssh --progress output/pdf/$l qgis.osgeo.osuosl.org:/var/www/qgisdata/QGIS-Documentation-2.6/live/html/pdf
-    rsync -hvrz -e ssh --progress output/html/$l qgis.osgeo.osuosl.org:/var/www/qgisdata/QGIS-Documentation-2.6/live/html
+    time /bin/bash ./docker-run.sh full LANG=$l
+    time rsync -hvrz -e ssh --progress output/pdf/$l qgis.osgeo.osuosl.org:/var/www/qgisdata/QGIS-Documentation-2.6/live/html/pdf
+    time rsync -hvrz -e ssh --progress output/html/$l qgis.osgeo.osuosl.org:/var/www/qgisdata/QGIS-Documentation-2.6/live/html
   done
