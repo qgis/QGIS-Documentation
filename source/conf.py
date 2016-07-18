4396e5e9c21b067ed1243854d6c106dcacf40020
Richard Duivenvoorde <richard@duif.net>
2016-07-18 22:53:28 +0200

    redo wrong addition

--- source/conf.py
+++ source/conf.py
@@ -1420,4 +1420,3 @@ nitpick_ignore = [('py:class', 'QAction'),
                   ('py:mod', 'qgis.gui'),
                   ('py:mod', 'qgis.utils'),
                   ]
-exclude_patterns += ['docs/user_manual/processing_algs/*']
