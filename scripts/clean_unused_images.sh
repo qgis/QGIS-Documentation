ed540b6ca7514d7381458b8890c25e53f577ae50
Alexandre Neto <senhor.neto@gmail.com>
2018-02-22 23:58:42 +0000

    Adapt clean_unused_images to new file structure

--- scripts/clean_unused_images.sh
+++ scripts/clean_unused_images.sh
@@ -5,8 +5,8 @@
 
 #Find all images in the resources folder
 echo "Searching for image files..."
-find ./resources/en -type f -regex ".*\.\(png\|PNG\|gif\|jpg\|jpeg\)" -printf "%f\n" | sort -u > all_images.txt
-
+find ./source -type f -regex ".*\.\(png\|PNG\|gif\|jpg\|jpeg\)" -printf "%f\n" | sort -u > all_images.txt
+find ./static -type f -regex ".*\.\(png\|PNG\|gif\|jpg\|jpeg\)" -printf "%f\n" | sort -u >> all_images.txt
 
 #Check for images that are used in the source
 echo "Checking what images are used..."
@@ -14,7 +14,7 @@ grep -r -hof all_images.txt source/ | sort -u > found.txt
 
 #Find images that are not used
 echo "Creating list of NOT used image files..."
-grep -vf found.txt all_images.txt | xargs -I filename find ./resources/en -name filename > not_found.txt
+grep -vf found.txt all_images.txt | xargs -I filename find ./source -name filename > not_found.txt
 
 #Remove images not used
 echo "Removing NOT used files..."
