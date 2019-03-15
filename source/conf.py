27b0699aadde7985c5b20d54d940ba9040970323
Richard Duivenvoorde <richard@duif.net>
2019-03-15 14:50:36 +0100

    add common images

--- source/conf.py
+++ source/conf.py
@@ -24,6 +24,10 @@ author = 'QGIS Authors'
 
 # -- General configuration ---------------------------------------------------
 
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = 'static/common/logo.png'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
@@ -55,4 +59,4 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['static']
