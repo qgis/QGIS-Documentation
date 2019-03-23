0e86edfa6a1ac35c3db53155107c8a3a9d80f009
Richard Duivenvoorde <richard@duif.net>
2019-03-23 16:53:00 +0100

    Adding rtd-theme-html options plus descriptions

--- source/conf.py
+++ source/conf.py
@@ -59,6 +59,41 @@ gettext_compact = False     # optional.
 #html_theme = 'alabaster'
 html_theme = 'rtd_qgis'
 
+# https://sphinx-rtd-theme.readthedocs.io/en/stable/configuring.html#theme-options
+# rtd / read the docs theme options:
+html_theme_options = {
+     # collapse_navigation: With this enabled, navigation entries are not expandable – the [+] icons next to each entry are removed. Default: True
+    'collapse_navigation': True,
+    # sticky_navigation: Scroll the navigation with the main page content as you scroll the page. Default: True
+    'sticky_navigation': True,
+    # navigation_depth: The maximum depth of the table of contents tree. Set this to -1 to allow unlimited depth. Default: 4
+    'navigation_depth': 4,
+    # includehidden:Specifies if the navigation includes hidden table(s) of contents – that is, any toctree directive that is marked with the :hidden: option. Default: True,
+    'includehidden': True,
+    # canonical_url: This will specify a canonical URL meta link element to tell search engines which URL should be ranked as the primary URL for your documentation. This is important if you have multiple URLs that your documentation is available through. The URL points to the root path of the documentation and requires a trailing slash.
+    'canonical_url': '',
+    # display_version: If True, the version number is shown at the top of the sidebar. Default: True,
+    'display_version': True,
+    # logo_only: Only display the logo image, do not display the project name at the top of the sidebar. Default: False,
+    'logo_only': False,
+    # prev_next_buttons_location': Location to display Next and Previous buttons. This can be either bottom, top, both , or None. Default: 'bottom',
+    'prev_next_buttons_location': 'bottom',
+    # style_external_links': Add an icon next to external links. Default: False,
+    'style_external_links': False,
+    # vcs_pageview_mode': Changes how to view files when using display_github, display_gitlab, etc. When using GitHub or GitLab this can be: blob (default), edit, or raw. On Bitbucket, this can be either: view (default) or edit. '',
+    # unsupported ??
+    #'vcs_pageview_mode': 'edit',
+    # style_nav_header_background': Changes the background of the search area in the navigation bar. The value can be anything valid in a CSS background property. Default: 'white',
+    #'style_nav_header_background': 'Gray',
+    # Toc options
+    # titles_only: When enabled, page subheadings are not included in the navigation. Default: False
+    'titles_only': False,
+    # 'github_url' Force the Edit on GitHub button to use the configured URL.
+     'github_url': 'https://github.com/rduivenvoorde/qgisdoc',
+    # 'gitlab_url' Force the Edit on GitLab button to use the configured URL.
+    # 'gitlab_url': 'xxx',
+}
+
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ['../themes']
 
