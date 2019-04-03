c80a218df3468ba78c29eeeb6f368914421c99f9
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2019-04-03 13:18:34 +0200

    Tests Doc 3.4 against QGIS 3.4
    And change doctest docker image tag to release-3_4.

--- docker.mk
+++ docker.mk
@@ -1,8 +1,8 @@
 # Makefile to run targets in docker containers
 
 doctest:
-	docker build -t qgis/documentation:latest -f doctest.dockerfile .
+	docker build -t qgis/documentation:release-3_4 -f doctest.dockerfile .
 	docker run --rm \
 		--user=$(shell id -u) \
 		--volume=$(PWD):/documentation \
-		qgis/documentation:latest
+		qgis/documentation:release-3_4
