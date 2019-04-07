3e07d6feaf1c0e4a4fffb3a5f352b6c0693263ef
Richard Duivenvoorde <richard@duif.net>
2019-04-07 18:19:19 +0200

    Adding languages in dockerfile

--- docker-world.sh
+++ docker-world.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# QGIS testing/master ONLY in english
-# translation is done only in latest stable
-
 # cd to script dir
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
 now=`date`
-echo "Starting $now"
+echo "Starting: $now"
 
 if [ -f running ]; then
 	echo "$0 still running"
@@ -24,17 +21,24 @@ git stash drop
 # get latest version
 git pull
 
-for LANG in en
+# only languages which have enough translations in transifex
+# nqo is not available in python :-(
+: ${langs:=en bg cs de es fi fr id it ja ko nl pt_BR pt_PT ro ru tr zh-Hant}
+
+# if you only want to build one language, do:
+# $ langs=de ./docker-world.sh
+
+for l in $langs
   do
-    time /bin/bash ./docker-run.sh full LANG=$LANG
+    time /bin/bash ./docker-run.sh full LANG=$l
     # we changed source/conf.py during build (exclude processing algs for pdf (see Makefile)
     # let's get a fresh checkout of source/conf.py
     git checkout source/conf.py
-    #time rsync -hvrzc -e ssh --progress output/pdf/$LANG qgis.osgeo.osuosl.org:/var/www/qgisdata/QGIS-Documentation/live/html/pdf
-    #time rsync -hvrzc -e ssh --progress output/html/$LANG qgis.osgeo.osuosl.org:/var/www/qgisdata/QGIS-Documentation/live/html
-    # 1 dec 2017 local rsync (moving site to qgis2)
-    time rsync -hvrzc --progress output/pdf/$LANG /var/www/qgisdata/QGIS-Documentation-3.4/live/html/pdf
-    time rsync -hvrzc --progress output/html/$LANG /var/www/qgisdata/QGIS-Documentation-3.4/live/html
+    #time rsync -hvrzc -e ssh --progress output/pdf/$l qgis.osgeo.osuosl.org:/var/www/qgisdata/QGIS-Documentation-2.18/live/html/pdf
+    #time rsync -hvrzc -e ssh --progress output/html/$l qgis.osgeo.osuosl.org:/var/www/qgisdata/QGIS-Documentation-2.18/live/html
+    # 1 dec 2017 rsycn to local disk
+    time rsync -hvrzc --progress output/pdf/$l /var/www/qgisdata/QGIS-Documentation-2.18/live/html/pdf
+    time rsync -hvrzc --progress output/html/$l /var/www/qgisdata/QGIS-Documentation-2.18/live/html
   done
 
 now=`date`
