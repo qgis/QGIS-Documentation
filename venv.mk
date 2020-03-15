e80379a1556270630d2a7df3805b23a5d616c716
Richard Duivenvoorde <richard@duif.net>
2020-03-15 16:28:04 +0100

    New Build Structure (copy of 3.10)
    Please test and report back.
    IF something is missing, please recover from git and put back.
    conf.py.old is still there, can be removed
    
    Build is for now down by hand on my laptop...
    Have to automate this later

--- venv.mk
+++ venv.mk
@@ -1,45 +1,27 @@
 # Makefile to create and use a python virtual environment on the fly
 
-# QGIS_PREFIX_PATH, used to determine relative paths.
-# Default to system QGIS installation
-QGIS_PREFIX_PATH ?= /usr
-
-# Include system packages (needed for PyQt)
-PYTHONPATH ?= /usr/lib/python3/dist-packages
-
-# Include QGIS binaries and Python binding
-ifneq ($(QGIS_PREFIX_PATH), "")
-LD_LIBRARY_PATH := $(QGIS_PREFIX_PATH)/lib:$(LD_LIBRARY_PATH)
-# For install folder
-PYTHONPATH := $(QGIS_PREFIX_PATH)/share/qgis/python:$(PYTHONPATH)
-PYTHONPATH := $(QGIS_PREFIX_PATH)/share/qgis/python/plugins:$(PYTHONPATH)
-# For build output folder
-PYTHONPATH := $(QGIS_PREFIX_PATH)/python:$(PYTHONPATH)
-PYTHONPATH := $(QGIS_PREFIX_PATH)/python/plugins:$(PYTHONPATH)
-endif
-
 # Use sphinx from virtualenv
-SPHINXBUILD   ?= QGIS_PREFIX_PATH=$(QGIS_PREFIX_PATH) LD_LIBRARY_PATH=$(LD_LIBRARY_PATH) PYTHONPATH=$(PYTHONPATH) venv/bin/sphinx-build
+SPHINXBUILD   ?= venv/bin/sphinx-build
 SPHINXINTL    ?= venv/bin/sphinx-intl
-
-# Delete all including the virtual environment
-cleanall: springclean
-	rm -rf venv
 
 # Create the venv and a timestamp file
 venv/venv.timestamp:
-	python3 -m venv venv
+	virtualenv -p python3 venv
 	touch $@
 
 # Install requirements and create a timestamp file
+# NOTE: we use --pre with pip install to have the not yet release 2.0 Sphinx
 venv/REQUIREMENTS.timestamp: venv/venv.timestamp REQUIREMENTS.txt
-	venv/bin/pip install -r REQUIREMENTS.txt
+	venv/bin/pip install --pre -r REQUIREMENTS.txt
 	touch $@
 
 include Makefile
+
+# Delete all including the 
+cleanall: springclean
+	rm -rf venv
 
 # Add dependencies to targets that require sphinx
 html: venv/REQUIREMENTS.timestamp
 gettext: venv/REQUIREMENTS.timestamp
 fasthtml: venv/REQUIREMENTS.timestamp
-doctest: venv/REQUIREMENTS.timestamp
