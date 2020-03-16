1f81c15c12d4430e9c63c15e0bb0762dfce53487
Richard Duivenvoorde <richard@duif.net>
2020-03-16 18:58:04 +0100

    BIG BANG to new toolchain and ReadTheDocs themes
    Downloaded all locales which we build for and put them in git (in locale folder)
    Used new toolchain to build and push 3.10 to docs.qgis.org

---
+++ user.mk
@@ -0,0 +1,7 @@
+# Root installation forder
+QGIS_PREFIX_PATH = /home/richard/bin/qgis/master/debug/
+
+# Or build output folder
+#QGIS_PREFIX_PATH = /home/user/dev/QGIS-build-master/output
+
+include venv.mk
