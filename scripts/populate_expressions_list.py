272362fee576074f904d2f9b2df4c8187d4e51b8
Alexandre Neto <senhor.neto@gmail.com>
2020-07-10 17:08:02 +0100

    Adds script to conver JSON to Function RST

---
+++ scripts/populate_expressions_list.py
@@ -0,0 +1,138 @@
+from os import path, walk
+import json
+
+# Determine the folder where the json files are
+qgis_repo_path = path.abspath('/home/aneto/qgis/repos/QGIS')
+help_folder = path.join(qgis_repo_path,'resources/function_help/json')
+# expression help folder
+output_folder = path.abspath('./docs/user_manual/working_with_vector/expression_help')
+
+def format_function(function_dict):
+    f_name = function_dict['name']
+    
+    # Organize arguments
+    arg_syntax_list = []
+    arg_description_list = []
+    variable_args = ''
+    if 'arguments' in function_dict:
+        for arg in function_dict['arguments']:
+            if 'syntaxOnly' in arg:
+                arg_syntax_list.append(arg['arg'])
+                variable_args = ', ...'
+            elif 'descOnly' in arg:
+                arg_description_list.append(f"**{arg['arg']}** - {arg['description']}")
+            else:
+                arg_syntax_list.append(arg['arg'])
+                arg_description_list.append(f"**{arg['arg']}** - {arg['description']}")
+    
+    # Prepare syntax and arguments strings
+    if len(arg_syntax_list) > 0:
+        syntax = f'{f_name}({", ".join(arg_syntax_list)}{variable_args})'
+        descriptions = '\n    '.join(arg_description_list)
+        arguments = (f"  - Arguments\n"
+                     f"  - {descriptions}\n"
+                     f"\n")
+    else:
+        syntax = f_name
+        arguments = ''    
+    
+    # Prepare examples
+    if 'examples' in function_dict:
+        ex_list = [f"``{ex['expression']}`` → {ex['returns']}" for ex in function_dict['examples']]
+        examples = "  - Examples\n  - "+ "\n    ".join(ex_list)
+    else:
+        examples = ''
+
+
+
+        
+    
+    f_description = function_dict['description']
+    f_examples_list = 'test2 test2 test2'
+    text = (f".. _expression_function_{f_name}:\n\n"
+            f"{f_name}\n"
+            f"{'.'* len(f_name)}\n"
+            f"\n"
+            f"{syntax}\n"
+            f"\n"
+            f".. list-table::\n"
+            f"   :widths: 15 85\n"
+            f"   :stub-columns: 1\n"
+            f"\n"
+            f"* - Description\n"
+            f"  - {f_description}\n"
+            f"\n"
+            f"{arguments}"
+            f"{examples}"
+            f"\n\n\n")
+    return text
+        
+# Get a list of all files on that folder
+
+(_, _, filenames) = next(walk(help_folder))
+
+# Store groups and functions in memory
+groups = {}
+functions = {}
+for file in filenames:
+    filepath = path.join(help_folder, file)
+    with open(filepath) as f:
+        data = json.load(f)
+    #print(data)
+    if data['type'] == 'group':
+        groups[data['name']] = {'description': data['description'], 'func_list': []}
+    elif data['type'] == 'function':
+        functions[data['name']] = data
+    else:
+        print(f'{data["name"]} is not a group or a function. It is a {data["type"]}')
+
+# Distribute functions by the groups
+for f_name in functions.keys():
+    function = functions[f_name]
+    if 'groups' in function:
+        for g_name in function['groups']:
+            if g_name not in ('deprecated'):
+                groups[g_name]['func_list'].append(function['name'])
+            else:
+                print(f'Warning: Function {function["name"]} is deprecated!')
+    else:
+        print(f'Warning: Function {function["name"]} has no group!')
+    #    print(group)
+
+for g_name in groups:
+    func_list = groups[g_name]['func_list']
+    func_list.sort()
+    output_group_file = path.join(output_folder,g_name + '.txt')
+    with open(output_group_file, 'w') as f:
+        for f_name in func_list:
+            text = format_function(functions[f_name])
+            f.write(text)
+'''
+... _expression_function_{f_name}:
+
+    {f_name}
+    {"."*len(f_name)}
+
+    *{color_cmyk} ({f_arguments_list})*
+
+    .. list-table::
+    :widths: 15 85
+    :stub-columns: 1
+
+    * - Description
+        - {f_description}
+
+    * - Arguments
+        - **{f_argument}** - {f_arguments_description}
+
+        **magenta** - magenta component of the color, as a percentage integer value from 0 to 100
+
+        **yellow** - yellow component of the color, as a percentage integer value from 0 to 100
+
+        **black** - black component of the color, as a percentage integer value from 0 to 100
+
+    * - Examples
+        - ``color_cmyk(100,50,0,10)`` → ``'0,115,230'``
+    ´´´
+    # close file
+'''
