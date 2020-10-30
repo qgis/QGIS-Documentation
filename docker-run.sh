02b00a2f22121b18c326564a757d87fbc530e191
Richard Duivenvoorde <richard@duif.net>
2020-10-30 18:42:34 +0100

    Docker scripts (temp using html)

--- docker-run.sh
+++ docker-run.sh
@@ -6,5 +6,5 @@
 
 cp ~/.transifexrc .
 pwd=$(pwd)
-docker run -v $pwd:/build -w="/build" --rm=true --name="qgis_docs_master_build" qgis/sphinx_pdf_3 make $@
+docker run -v $pwd:/build -w="/build" --rm=true --name="qgis_docs_master_build" qgis/sphinx_html_3 make $@
 rm -rf .transifexrc
---
+++ docker-world.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# cd to script dir
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+now=`date`
+echo "Starting: $now"
+
+if [ -f running ]; then
+	echo "$0 still running"
+	exit 1
+fi
+
+touch running
+trap "rm $PWD/running" EXIT
+
+TARGET=${1:-full}
+
+# throw away building artefacts
+git stash
+git stash drop
+# get latest version
+git pull
+
+# only languages which have translations in transifex
+#: ${langs:=en ca da de es fa fi fr gl hu id it ja km_KH ko lt nl pl pt_BR pt_PT ro tr ru uk zh-Hans zh-Hant}
+: ${langs:=en}
+
+# if you only want to build one language, do:
+# $ langs=de ./docker-world.sh
+
+for l in $langs
+  do
+    ./docker-run.sh $TARGET LANG=$l
+    build_ok=$?
+    if [[ "$build_ok" = "0" ]]; then
+      echo "Build OK: syncing to web"
+      #time rsync -hvrzc -e ssh --progress output/html/$l qgis.osgeo.osuosl.org:/var/www/qgisdata/QGIS-Website/live/html
+      # 1 dec 2017: sync to qgis2
+      time rsync -hvrzc --delete --progress output/html/$l /var/www/qgisdata/QGIS-Documentation/live/html
+    else
+      echo "Build FAILED: not syncing to web";
+    fi
+  done
+
+now=`date`
+echo "Finished: $now"
