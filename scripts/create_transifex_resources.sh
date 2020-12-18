c133a7dc30c0312fe2dab0d32fa431d62af91366
Harrissou Sant-anna <delazj@gmail.com>
2020-12-18 14:27:28 +0100

    Update the script to generate the transifex config file
    - clean and update file references
    - use formatting compatible with transifex-github integration
    - use more recent tx config mapping command

--- scripts/create_transifex_resources.sh
+++ scripts/create_transifex_resources.sh
@@ -1,67 +1,57 @@
 #!/bin/bash
 
-# This script is used to register InaSAFE translatable resources with Transifex
-# http://transifex.com
+# This script is used to register QGIS-Documentation translatable resources with Transifex
+# https://www.transifex.com
 #
 # Note that this script updates or creates entries in .tx/config file
+# so that they match expected formatting of tx-github integration slugs.
+# Used to first "tx push" existing translations to Transifex when enabling the github integration
 #
 # Tim Sutton, March 2013
+# Update: Harrissou Sant-anna, December 2020
 
-#
-# Sphinx documentation first
-#
+TARGETBRANCH='master'
+SOURCEPOFILES='locale/en/LC_MESSAGES/docs/'
+PROJECT='qgis-documentation'
 
-#LOCALES=`ls i18n`
-LOCALES='de es fi fr gl hi id ja it ko nl nqo pl pt_BR pt_PT ro ru'
+# To be sure there are no *.pot or *.mo files left
+make springclean
 
-# to be sure there are no pot files left
-make clean
+# Regenerate the English PO files
+make gettext
+rm -r $SOURCEPOFILES
+sphinx-intl update -p build/gettext -l en
 
-for POFILE in `find i18n/en/LC_MESSAGES/${ITEM}/ -type f -name '*.po'`
+# Clean the .tx/config files from existing references, out of the main section
+sed -i "/$PROJECT/,$ d" .tx/config
+
+for POFILE in `find $SOURCEPOFILES -type f -name '*.po'`
 do
+  #echo $POFILE
   # get the po file, replacing 'en' with '<lang>' and removing double '//'s in path
-  GENERICFILE=`echo $POFILE | sed 's/\/en\//\/<lang>\//g' | sed 's/\/\//\//g'`
-  #echo $GENERICFILE
-  # Get the filename-only part of the po file so we can use that
-  # name when registering the resource
-  BASE=`dirname $GENERICFILE`/`basename $GENERICFILE .po`
-  # removing i18n/<lang>/LC_MESSAGES
-  # replace _ in - in filenames
-  # replace empty spaces to -
-  # replace / in _ 
-  # replace . to _ (eg for release2.0 names)
-  # so we have for a file like
-  #   site/about/my_screenshots/index.po
+  GENERICFILE=`echo $POFILE | sed 's,\/en\/,\/<lang>\/,g' | sed 's,\/\/,\/,g'`
+  echo $GENERICFILE
+
+  # Set the resource slug by
+  # lowering the case of 'LC_MESSAGES'
+  # appending the target branch name after double "-"
+  # and replacing "_", "/", "." and " " characters with "-" in the path
+  # so for a file like
+  #   locale/en/LC_MESSAGES/docs/gentle_gis_introduction/data_capture.po in release_3.16 branch
   # we will get
-  #   site_about_my-screenshots_index
-  BASE=`echo $BASE | sed 's,i18n.*LC_MESSAGES\/,,g' | sed 's,_,-,g' | sed 's, ,-,g' | sed 's,/,_,g' | sed 's,\.,-,g'`
-  RESOURCE=qgis-documentation.$BASE
-  #echo $RESOURCE
+  #   locale-en-lc-messages-docs-gentle-gis-introduction-data-capture-po--release-3-16
+  RESOURCE=`echo "$POFILE--$TARGETBRANCH" | sed 's,LC_MESSAGES,lc-messages,g' | sed 's,_,-,g' | sed 's, ,-,g' | sed 's,/,-,g' | sed 's,\.,-,g'`
+  echo $RESOURCE
   # Register each po file as a transifex resource (an individual translatable file)
   set -x
-  tx set -t PO --auto-local -r $RESOURCE \
-    "$GENERICFILE" \
-    --source-lang en \
+  tx config mapping \
+    -t PO \
+    -r $PROJECT.$RESOURCE \
+    -s $POFILE \
+    --expression "$GENERICFILE" \
+    --source-language en \
     --execute
-  #set +x
-  # Now register the language translations for the localised po file against
-  # this resource.
-  for LOCALE in $LOCALES
-  do
-      LOCALEFILE=`echo $POFILE | sed "s/\/en\//\/$LOCALE\//g"`
-      #echo "$LOCALEFILE"
-      tx set -r $RESOURCE -l $LOCALE  "$LOCALEFILE"
-  done
-  # When we are done in this block we should have created a section in the
-  # .tx/config file that looks like this:
-  #
-  #   [qgis-website.site_forusers_alldownloads]
-  #   file_filter = i18n/<lang>/LC_MESSAGES/site/forusers/alldownloads.po
-  #   source_file = i18n/en/LC_MESSAGES/site/forusers/alldownloads.po
-  #   source_lang = en
-  #   trans.nl = i18n/nl/LC_MESSAGES/site/forusers/alldownloads.po
-  #   type = PO
 done
 
-#Print out a listing of all registered resources
+# Print out a listing of all registered resources
 #tx status
