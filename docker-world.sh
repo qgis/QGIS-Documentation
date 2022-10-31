8d24297591968932398d0590d967438afdc8fe02
Richard Duivenvoorde <richard@duif.net>
2022-10-31 21:19:57 +0100

    Copy 3.22 version and change for 3.28

--- docker-world.sh
+++ docker-world.sh
@@ -24,7 +24,8 @@ git stash drop
 git pull
 
 # only languages which have translations in transifex
-#: ${langs:=en ca da de es fa fi fr gl hu id it ja km_KH ko lt nl pl pt_BR pt_PT ro tr ru uk zh-Hans zh-Hant}
+# NOTE: we BUILD zh_Hans, but we SERVE zh-Hans
+: ${langs:=en cs de es fr hu it ja ko li nl pt_BR pt_PT ro ru zh_Hans}
 : ${langs:=en}
 
 # if you only want to build one language, do:
@@ -36,15 +37,10 @@ for l in $langs
     build_ok=$?
     if [[ "$build_ok" = "0" ]]; then
       echo "Build OK: syncing to web"
-      #time rsync -hvrzc -e ssh --progress build/html/$l qgis.osgeo.osuosl.org:/var/www/qgisdata/QGIS-Website/live/html
-      # 1 dec 2017: local sync to qgis2
-      #time rsync -hvrzc --delete --progress build/html/$l /var/www/qgisdata/QGIS-Documentation/live/html
-      #time rsync -hvrzc --delete --progress build/pdf /var/www/qgisdata/QGIS-Documentation/live/html
-      #time rsync -hvrzc --delete --progress build/zip /var/www/qgisdata/QGIS-Documentation/live/html
-      # 20220317: local sync to new www2
-      time rsync -hvrzc --delete --progress build/html/$l root@195.201.96.242:/var/www/qgisdata/QGIS-Documentation-3.28/live/html
-      time rsync -hvrzc --delete --progress build/pdf root@195.201.96.242:/var/www/qgisdata/QGIS-Documentation-3.28/live/html
-      time rsync -hvrzc --delete --progress build/zip root@195.201.96.242:/var/www/qgisdata/QGIS-Documentation-3.28/live/html
+      # 20220317 sync to remote www2
+      time rsync -hvrzc --delete --progress build/html/$l www2.qgis.org:/var/www/qgisdata/QGIS-Documentation-3.28/live/html
+      time rsync -hvrzc --delete --progress build/pdf www2.qgis.org:/var/www/qgisdata/QGIS-Documentation-3.28/live/html
+      time rsync -hvrzc --delete --progress build/zip www2.qgis.org:/var/www/qgisdata/QGIS-Documentation-3.28/live/html
     else
       echo "Build FAILED: not syncing to web";
     fi
