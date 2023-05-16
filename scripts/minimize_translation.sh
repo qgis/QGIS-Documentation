023b4b82d938f929152c5099a08079b258e1dfbe
Harrissou Sant-anna <delazj@gmail.com>
2023-05-16 10:20:07 +0200

    Update minimize_translation.sh
    to match master

--- scripts/minimize_translation.sh
+++ scripts/minimize_translation.sh
@@ -14,30 +14,32 @@ LANG=${*:--a}
 
 # list of target folders
 SOURCEPOFILES='locale'
-#ENGLISHPOFILES='locale/pofiles_count_string.txt'
-#UNTRANSLATEDPOFILES='locale/about_to_delete.txt'
-#rm -rf $ENGLISHPOFILES  $UNTRANSLATEDPOFILES
+# ENGLISHPOFILES='locale/pofiles_count_string.txt'
+# UNTRANSLATEDPOFILES='locale/about_to_delete.txt'
+# rm -rf $ENGLISHPOFILES  $UNTRANSLATEDPOFILES
 
 # Pull translations of all languages from Transifex,
 # fetching only the strings that are really translated and not the whole file
-./tx pull --force --mode onlytranslated $LANG
+tx pull --use-git-timestamps --mode onlytranslated --workers 15 $LANG
 
 # The onlytranslated mode actually pulls all the existing files
 # (with only the file header when no string has been translated yet)
 # so let's remove those files that have no translated string (except for English)
 for POFILE in `find $SOURCEPOFILES -type f -not -path "*/en/*" -name '*.po'`
 do
-  # if only 'msgstr ""' appears in the file (with no text between quotes),
-  # then there's not a single translated string in it
-  # so let's find those files and delete them
-  COUNT=`grep -oce 'msgstr ".\+"' $POFILE`
-  #echo "$POFILE: $COUNT" >>  $ENGLISHPOFILES
-  if [ $COUNT -eq 0 ]; then \
-    #echo "$POFILE" >> $UNTRANSLATEDPOFILES;\
+  # Todo: Also remove the Tx notranslate strings from pulled strings?
+
+  # If only msgstr with no text between quotes next to it or on next line appears in the file,
+  # then there's not a single human translated string in the file
+  # so let's find those files and delete them.
+  # For safety we catch the whole block including lines number, original and translated strings
+  COUNT=`grep -Pazo '(#:.*\n)+msgid (".*"\n)+msgstr (".+"|""\n(".+"\n)+)' $POFILE | wc -l`
+  # echo "$POFILE: $COUNT" >>  $ENGLISHPOFILES
+  if [[ $COUNT -lt 1 ]]; then \
+    # echo "$POFILE to delete" >> $UNTRANSLATEDPOFILES;\
     rm "$POFILE"
   fi;
 done
 
-#remove the resulting empty folders
+# remove the resulting empty folders
 find $SOURCEPOFILES -type d -empty -delete
-
