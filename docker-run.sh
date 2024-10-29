f6136728b099f8a0cdf066638585d4a31b338751
DelazJ <delazj@gmail.com>
2024-10-29 11:15:11 +0100

    Base docker build commands on their target docs version
    These commands are meant to be run on our servers to build the different releases.
    Instead of manually adjusting them at every new release, let's automatize using the target release as variable

--- docker-run.sh
+++ docker-run.sh
@@ -6,5 +6,9 @@
 
 cp ~/.transifexrc .
 pwd=$(pwd)
-docker run -v $pwd:/build -w="/build" --rm=true --name="qgis_docs_master_build" qgis/sphinx_pdf_3 make $@
+
+# Return release number from branch name e.g. master, 3.34
+TARGETBRANCH=`git branch --show-current | sed 's,release_,,g'`
+
+docker run -v $pwd:/build -w="/build" --rm=true --name="qgis_docs_"$TARGETBRANCH"_build" qgis/sphinx_pdf_3 make $@
 rm -rf .transifexrc
