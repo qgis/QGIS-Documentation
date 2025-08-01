f01d9705999ada8401f9043a7731dbef0689e764
viperminiq <viperminiq@gmail.com>
2025-08-01 10:57:12 +0200

    add PYTHONPATH

--- docker.mk
+++ docker.mk
@@ -4,6 +4,7 @@ doctest:
 	docker build -t qgis/documentation:latest -f doctest.dockerfile .
 	docker run --rm \
 		--env=QGIS_PREFIX_PATH=/usr \
+		--env=PYTHONPATH=/usr/share/qgis/python \
 		--user=$(shell id -u) \
 		--volume=$(PWD):/documentation \
 		qgis/documentation:latest
